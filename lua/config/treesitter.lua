539f7a3919e3650b109514ca829e689aee06f1af
lfanew <tyler.bowers@outlook.com>
2025-03-09 20:19:24 -0500

    added zig to treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 -- treesitter.lua (in lua/config/treesitter.lua)
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "d", "lua", "c", "cpp", "python" },
+  ensure_installed = { "zig", "d", "lua", "c", "cpp", "python" },
   highlight = {
     enable = true,
   },
